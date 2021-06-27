d37e3f668bce4f8fac8a111fcd374be5f00c0c4f
琴梨梨 <yeyaohua2010@163.com>
2021-06-27 20:39:44 +0800

    Add files via upload

---
+++ MethanolVer/ctfile.js
@@ -0,0 +1,70 @@
+window.removeEventListener('error', windowErrorHandler);
+setTimeout(Kiyomi.jsSetTitle("城通网盘解析器"),0);
+document.body.innerHTML = Base64.decode("PEgyPuivt+i+k+WFpemTvuaOpeWSjOWvhueggSjlj6/pgIkpPC9IMj4KICAgIDxIND7kuIvovb3lnLDlnYDmnInkuIDlrprmnInmlYjmnJ8s5LiN5Y+v55So5LqO5YiG5LqrPC9IND4KICAgIDxsYWJlbCBzdHlsZT0id2lkdGg6MTAwJTsiIGNsYXNzPSJtZGMtdGV4dC1maWVsZCBtZGMtdGV4dC1maWVsZC0tb3V0bGluZWQiIGRhdGEtbWRjLWF1dG8taW5pdD0iTURDVGV4dEZpZWxkIj4KICAgICAgICA8aW5wdXQgaWQ9ImxpbmsiIGNsYXNzPSJtZGMtdGV4dC1maWVsZF9faW5wdXQiIGFyaWEtbGFiZWxsZWRieT0ibXktbGFiZWwtaWQiPgogICAgICAgIDxzcGFuIGNsYXNzPSJtZGMtbm90Y2hlZC1vdXRsaW5lIj4KICAgICAgICAgICAgPHNwYW4gY2xhc3M9Im1kYy1ub3RjaGVkLW91dGxpbmVfX2xlYWRpbmciPjwvc3Bhbj4KICAgICAgICAgICAgPHNwYW4gY2xhc3M9Im1kYy1ub3RjaGVkLW91dGxpbmVfX25vdGNoIj4KICAgICAgICAgICAgICAgIDxzcGFuIGNsYXNzPSJtZGMtZmxvYXRpbmctbGFiZWwiIGlkPSJteS1sYWJlbC1pZCI+5YiG5Lqr6ZO+5o6lPC9zcGFuPgogICAgICAgICAgICA8L3NwYW4+CiAgICAgICAgICAgIDxzcGFuIGNsYXNzPSJtZGMtbm90Y2hlZC1vdXRsaW5lX190cmFpbGluZyI+PC9zcGFuPgogICAgICAgIDwvc3Bhbj4KICAgIDwvbGFiZWw+CiAgICA8YnI+CiAgICA8YnI+CiAgICA8bGFiZWwgc3R5bGU9IndpZHRoOjEwMCU7IiBjbGFzcz0ibWRjLXRleHQtZmllbGQgbWRjLXRleHQtZmllbGQtLW91dGxpbmVkIiBkYXRhLW1kYy1hdXRvLWluaXQ9Ik1EQ1RleHRGaWVsZCI+CiAgICAgICAgPGlucHV0IGlkPSJwYXNzY29kZSIgY2xhc3M9Im1kYy10ZXh0LWZpZWxkX19pbnB1dCIgYXJpYS1sYWJlbGxlZGJ5PSJteS1sYWJlbC1pZCI+CiAgICAgICAgPHNwYW4gY2xhc3M9Im1kYy1ub3RjaGVkLW91dGxpbmUiPgogICAgICAgICAgICA8c3BhbiBjbGFzcz0ibWRjLW5vdGNoZWQtb3V0bGluZV9fbGVhZGluZyI+PC9zcGFuPgogICAgICAgICAgICA8c3BhbiBjbGFzcz0ibWRjLW5vdGNoZWQtb3V0bGluZV9fbm90Y2giPgogICAgICAgICAgICAgICAgPHNwYW4gY2xhc3M9Im1kYy1mbG9hdGluZy1sYWJlbCIgaWQ9Im15LWxhYmVsLWlkIj7lr4bnoIEo55WZ56m65YiZ5L2/55So55C05qKo5qKo6YCa55So5a+G56CBKTwvc3Bhbj4KICAgICAgICAgICAgPC9zcGFuPgogICAgICAgICAgICA8c3BhbiBjbGFzcz0ibWRjLW5vdGNoZWQtb3V0bGluZV9fdHJhaWxpbmciPjwvc3Bhbj4KICAgICAgICA8L3NwYW4+CiAgICA8L2xhYmVsPgogICAgPGJyPgogICAgPGJyPgogICAgPGJ1dHRvbiBvbmNsaWNrPSJnZXRJbmZvKCk7IiBjbGFzcz0ibWRjLWJ1dHRvbiBmb28tYnV0dG9uMSI+CiAgICAgICAgPGRpdiBjbGFzcz0ibWRjLWJ1dHRvbl9fcmlwcGxlIj48L2Rpdj4KICAgICAgICA8c3BhbiBjbGFzcz0ibWRjLWJ1dHRvbl9fbGFiZWwiPuiOt+WPluS4i+i9veWcsOWdgDwvc3Bhbj4KICAgIDwvYnV0dG9uPgogICAgPGJyPgogICAgPEg0IGlkPSJmaWxlbmFtZSI+PC9IND4KICAgIDxINCBpZD0iZmlsZXNpemUiPjwvSDQ+CiAgICA8SDQgaWQ9ImZpbGV0aW1lIj48L0g0PgogICAgPHAgaWQ9ImRsVVJMIj48L3A+CiAgICA8YnV0dG9uIG9uY2xpY2s9ImRsRmlsZSgpOyIgaWQ9ImRsYnRuIiBzdHlsZT0iZGlzcGxheTpub25lOyIgY2xhc3M9Im1kYy1idXR0b24gZm9vLWJ1dHRvbjIiPgogICAgICAgIDxkaXYgY2xhc3M9Im1kYy1idXR0b25fX3JpcHBsZSI+PC9kaXY+CiAgICAgICAgPHNwYW4gY2xhc3M9Im1kYy1idXR0b25fX2xhYmVsIj7kuIvovb3mlofku7Y8L3NwYW4+CiAgICA8L2J1dHRvbj4=");
+var link;
+        var fileid;
+        var jsonurl;
+        var passcodeArgu="";
+        var jsonText;
+        var jsonText2;
+        var xhr;
+        var dlURL;
+        var getInfo = function () {
+            link = document.getElementById("link").value;
+            fileid = link.slice(link.indexOf("/f/") + 3);
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    jsonText = JSON.parse(xhr.responseText);
+                    if(jsonText.code==200){
+                    document.getElementById("filename").innerText = "文件名称:" + jsonText.file_name;
+                    document.getElementById("filesize").innerText = "文件大小:" + jsonText.file_size;
+                    document.getElementById("filetime").innerText = "文件时间:" + jsonText.file_time;
+                    document.getElementById("dlURL").innerText = "正在获取下载地址...";
+                    getURL();
+                    }else{
+                        document.getElementById("filename").innerText = "文件请求失败,请检查地址或视力";
+                    }
+                }
+            };
+            if (document.getElementById("passcode").value) {
+                passcodeArgu = "&passcode=" + document.getElementById("passcode").value;
+            }else{
+                passcodeArgu = "&passcode=547873715"
+            }
+            jsonurl = "https://webapi.ctfile.com/getfile.php?path=f&f=" + fileid + passcodeArgu + "&token=false&r=" + Math.random() + "&ref=https://ctfile.qinlili.bid"
+            xhr.open('get', jsonurl);
+            //xhr.setRequestHeader('X-Referer', link);
+            xhr.send(null);
+        }
+        var getURL = function () {
+            jsonurl = "https://webapi.ctfile.com/get_file_url.php?uid=" + jsonText.userid + "&fid=" + jsonText.file_id + "&file_chk=" + jsonText.file_chk + "&app=0&acheck=2&rd=" + Math.random()
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    jsonText2 = JSON.parse(xhr.responseText);
+                    dlURL = jsonText2.downurl;
+                    document.getElementById("dlURL").innerText = "下载地址:" + dlURL;
+                    document.getElementById("dlbtn").style.display = "block";
+                }
+            };
+            xhr.open('get', jsonurl);
+            xhr.send(null);
+        }
+        var dlFile = function () {
+            funDownload(dlURL, jsonText.file_name);
+        }
+
+window.mdc.autoInit();
+        mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button1'));
+        mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button2'));
+        function funDownload(content, filename) {
+
+            var eleLink = document.createElement('a');
+            eleLink.download = filename;
+            eleLink.style.display = 'none';
+            eleLink.href = content;
+            document.body.appendChild(eleLink);
+            eleLink.click();
+            document.body.removeChild(eleLink);
+        };
