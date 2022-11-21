0d71d5baf8ca6134cc12af583b528d2c77a0a7a1
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-11-21 21:13:08 +1000

    2.2.0 支持登录后高速解析!

--- ctget.js
+++ ctget.js
@@ -1,9 +1,9 @@
 window.ctfile = {
-    version: () => { return "2.5.0" },
-    getByLink: (link, password, firstcallback) => {
-        return ctfile.getByID(link.substring(link.lastIndexOf("/") + 1, (link.lastIndexOf("?") == -1) ? undefined : link.lastIndexOf("?")), (link.lastIndexOf("p=") == -1) ? password : link.substring(link.lastIndexOf("p=") + 2), firstcallback);
+    version: () => { return "2.6.0" },
+    getByLink: (link, password, token, firstcallback) => {
+        return ctfile.getByID(link.substring(link.lastIndexOf("/") + 1, (link.lastIndexOf("?") == -1) ? undefined : link.lastIndexOf("?")), (link.lastIndexOf("p=") == -1) ? password : link.substring(link.lastIndexOf("p=") + 2), token, firstcallback);
     },
-    getByID: async (fileid, password, firstcallback) => {
+    getByID: async (fileid, password, token, firstcallback) => {
         const origin = () => {
             //兼容node.js
             if (document && !(document.location.origin == 'file://')) {
@@ -23,7 +23,7 @@ window.ctfile = {
                 }
             }
         }
-        jsonText = JSON.parse(await (await fetch("https://webapi.ctfile.com/getfile.php?path=" + path(fileid) + "&f=" + fileid + "&passcode=" + password + "&token=false&r=" + Math.random() + "&ref=" + origin(), {
+        jsonText = JSON.parse(await (await fetch("https://webapi.ctfile.com/getfile.php?path=" + path(fileid) + "&f=" + fileid + "&passcode=" + password + "&token=" + token + "false&r=" + Math.random() + "&ref=" + origin(), {
             "headers": {
                 "origin": origin(),
                 "referer": origin()
@@ -35,28 +35,48 @@ window.ctfile = {
                 "size": jsonText.file.file_size,
                 "time": jsonText.file.file_time,
             })
-            jsonText2 = JSON.parse(await (await fetch("https://webapi.ctfile.com/get_file_url.php?uid=" + jsonText.file.userid + "&fid=" + jsonText.file.file_id + "&file_chk=" + jsonText.file.file_chk + "&app=0&acheck=2&rd=" + Math.random(), {
-                "headers": {
-                    "origin": origin(),
-                    "referer": origin()
-                },
-            })).text());
-            if (jsonText2.code == 200) {
+            if (jsonText.file.is_vip == 1) {
                 return {
                     "success": true,
                     "name": jsonText.file.file_name,
                     "size": jsonText.file.file_size,
                     "time": jsonText.file.file_time,
-                    "link": jsonText2.downurl
+                    "link": jsonText.file.vip_dx_url
                 };
             } else {
-                return {
-                    "success": false,
-                    "name": jsonText.file.file_name,
-                    "size": jsonText.file.file_size,
-                    "time": jsonText.file.file_time,
-                    "errormsg": jsonText2.message
-                };
+                jsonText2 = JSON.parse(await (await fetch("https://webapi.ctfile.com/get_file_url.php?uid=" + jsonText.file.userid + "&fid=" + jsonText.file.file_id + "&file_chk=" + jsonText.file.file_chk + "&app=0&acheck=2&rd=" + Math.random(), {
+                    "headers": {
+                        "origin": origin(),
+                        "referer": origin()
+                    },
+                })).text());
+                if (jsonText2.code == 200) {
+                    return {
+                        "success": true,
+                        "name": jsonText.file.file_name,
+                        "size": jsonText.file.file_size,
+                        "time": jsonText.file.file_time,
+                        "link": jsonText2.downurl
+                    };
+                } else {
+                    if (jsonText2.code == 302) {
+                        return {
+                            "success": false,
+                            "name": jsonText.file.file_name,
+                            "size": jsonText.file.file_size,
+                            "time": jsonText.file.file_time,
+                            "errormsg": "需要登录！"
+                        };
+                    } else {
+                        return {
+                            "success": false,
+                            "name": jsonText.file.file_name,
+                            "size": jsonText.file.file_size,
+                            "time": jsonText.file.file_time,
+                            "errormsg": jsonText2.message
+                        };
+                    }
+                }
             }
         } else {
             return {
