b751cc5f943165da91e32180908cee09925e2d75
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2024-08-24 10:50:11 +1000

    支持节点选择

--- ctget.js
+++ ctget.js
@@ -8,12 +8,10 @@
             return token;
         },
         getByLink: (link, password, token, firstcallback) => {
+            console.warn("ctfile.getByLink is deprecated, use ctfile.getByID instead.");
             return ctfile.getByID(link.substring(link.lastIndexOf("/") + 1, (link.lastIndexOf("?") == -1) ? undefined : link.lastIndexOf("?")), (link.lastIndexOf("p=") == -1) ? password : link.substring(link.lastIndexOf("p=") + 2), token, firstcallback);
         },
-        getByID: async (fileid, password, token, firstcallback) => {
-            if (!token) {
-                token = ctfile.buildToken();
-            }
+        getByID: async (fileid, password, token, firstCallback = ctfile.buildToken(), preferNode = "dx") => {
             const origin = () => {
                 //兼容node.js
                 if (document && !(document.location.origin == 'file://')) {
@@ -40,20 +38,53 @@
                 },
             })).text());
             if (jsonText.code == 200) {
-                if (firstcallback) {
-                    firstcallback({
+                if (firstCallback) {
+                    firstCallback({
                         "name": jsonText.file.file_name,
                         "size": jsonText.file.file_size,
                         "time": jsonText.file.file_time,
                     })
                 };
                 if (jsonText.file.is_vip == 1) {
+                    let url = false;
+                    switch (preferNode) {
+                        case "dx":
+                            if (jsonText.file.vip_dx_url) {
+                                url = jsonText.file.vip_dx_url;
+                                break;
+                            };
+                        case "yd":
+                            if (jsonText.file.vip_yd_url) {
+                                url = jsonText.file.vip_yd_url;
+                                break;
+                            }
+                        case "lt":
+                            if (jsonText.file.vip_lt_url) {
+                                url = jsonText.file.vip_lt_url;
+                                break;
+                            }
+                        default:
+                        case "us":
+                            if (jsonText.file.us_downurl_a) {
+                                url = jsonText.file.us_downurl_a;
+                                break;
+                            }
+                    }
+                    if (!url) {
+                        return {
+                            "success": false,
+                            "name": jsonText.file.file_name,
+                            "size": jsonText.file.file_size,
+                            "time": jsonText.file.file_time,
+                            "errormsg": "解析结果没有可用的链接！"
+                        };
+                    }
                     return {
                         "success": true,
                         "name": jsonText.file.file_name,
                         "size": jsonText.file.file_size,
                         "time": jsonText.file.file_time,
-                        "link": jsonText.file.vip_dx_url
+                        "link": url
                     };
                 } else {
                     jsonText2 = JSON.parse(await (await fetch("https://webapi.ctfile.com/get_file_url.php?uid=" + jsonText.file.userid + "&fid=" + jsonText.file.file_id + "&file_chk=" + jsonText.file.file_chk + "&app=0&acheck=2&rd=" + Math.random(), {
